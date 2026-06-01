6881a21a8409253f4b0dce9d2aa98abad0b807da
jaehong lee <jhljjg071483@gmail.com>
2026-06-01 14:55:52 +0900

    as

--- src/app.js
+++ src/app.js
@@ -3004,7 +3004,16 @@ function getTimestamp() {
     pad(d.getSeconds())
   );
 }
+Date.prototype.YYYYMMDDHHMMSS = function () {
+  var yyyy = this.getFullYear().toString();
+  var MM = pad(this.getMonth() + 1, 2);
+  var dd = pad(this.getDate(), 2);
+  var hh = pad(this.getHours(), 2);
+  var mm = pad(this.getMinutes(), 2);
+  var ss = pad(this.getSeconds(), 2);
 
+  return yyyy + MM + dd + hh + mm + ss;
+};
 app.post("/api/reservation/refund", async (req, res) => {
   const conn = await pool.getConnection();
 
