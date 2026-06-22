ed639c4a2c2544b9feb75bd8bf3edc5c2f19cb06
jaehong lee <jhljjg071483@gmail.com>
2026-06-22 15:12:32 +0900

    a

--- src/app.js
+++ src/app.js
@@ -1169,25 +1169,18 @@ app.get("/api/get-main-event-popup", async (req, res) => {
 
 app.post("/api/room-price", async (req, res) => {
   try {
-<<<<<<< HEAD
     const { dates, rooms } = req.body;
 
     console.log("dates:", dates);
     console.log("rooms:", rooms);
 
     if (!Array.isArray(dates) || dates.length === 0) {
-=======
-    const { prices } = req.body;
-    console.log(prices);
-    if (!Array.isArray(prices) || prices.length === 0) {
->>>>>>> 2241fc74766d7f95d765f1203c2f0176b83e68d5
       return res.status(400).json({
         ok: false,
         message: "저장할 날짜 데이터가 없습니다.",
       });
     }
 
-<<<<<<< HEAD
     if (!Array.isArray(rooms) || rooms.length === 0) {
       return res.status(400).json({
         ok: false,
@@ -1212,19 +1205,6 @@ app.post("/api/room-price", async (req, res) => {
     });
 
     console.log("insert values:", values);
-=======
-    const values = prices.map((item) => [
-      item.date,
-      item.room_group_id,
-      item.room_group_name,
-      Number(item.price || 0),
-      Number(item.day_use_price || 0),
-      Number(item.human_plus_price || 0),
-      Number(item.pet_plus_price || 0),
-    ]);
-    
-    console.log(values);
->>>>>>> 2241fc74766d7f95d765f1203c2f0176b83e68d5
 
     await pool.query(
       `
